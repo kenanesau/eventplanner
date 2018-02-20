363dcd482dc84ae7c62079e1b3675c81b215e7b3
Kenan Esau <kenan.esau@conan.de>
2018-02-20 19:26:02 +0100

    - Fix of AuthenticationServiceImpl -- stored auth-status in Session

--- src/main/java/com/privatesecuredata/eventplanner/services/AuthenticationServiceImpl.java
+++ src/main/java/com/privatesecuredata/eventplanner/services/AuthenticationServiceImpl.java
@@ -1,24 +1,48 @@
 package com.privatesecuredata.eventplanner.services;
 
-public class AuthenticationServiceImpl implements AuthenticationService {
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.Session;
 
-	private boolean isAuthenticated = false;
+public class AuthenticationServiceImpl implements AuthenticationService {
+	
+	private static final String IS_AUTHENTICATED = "is_authenticated";
+
+	@Inject
+	private Request request;
+	
+	protected Boolean getAuthenticated() {
+		Session httpSession = request.getSession(true);
+		Boolean authenticated = (Boolean)httpSession.getAttribute(IS_AUTHENTICATED);
+		if (null == authenticated) {
+			authenticated = false;
+			httpSession.setAttribute(IS_AUTHENTICATED, authenticated);
+		}
+		
+		return authenticated;
+	}
+	
+	protected void setAuthenticated(Boolean authenticated) {
+		Session httpSession = request.getSession(true);
+		httpSession.setAttribute(IS_AUTHENTICATED, authenticated);
+	}
 	
 	@Override
 	public boolean isAuthenticated() {
-		return isAuthenticated;
+		return getAuthenticated();
 	}
 
 	@Override
 	public boolean authenticate(String username, String password) {
-		isAuthenticated = (username.equals("kenan.esau@conan.de") &&
-							password.equals("givemeajob"));
+		Boolean isAuthenticated = (username.equals("kenan.esau@conan.de") &&
+									password.equals("givemeajob"));
+		setAuthenticated(isAuthenticated);
 
 		return isAuthenticated;
 	}
 	
 	public void logout() {
-		if (isAuthenticated)
-			isAuthenticated = false;
+		if (getAuthenticated())
+			setAuthenticated(false);
 	}
 }
